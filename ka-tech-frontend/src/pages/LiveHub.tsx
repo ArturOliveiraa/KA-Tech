2e81f8ff9f290b0d4fddff753dbf5bc59c3a784b
Artur Oliveira <contato.arturoalves@gmail.com>
2026-01-31 10:28:14 -0300

    corrigir logo

--- ka-tech-frontend/src/pages/LiveHub.tsx
+++ ka-tech-frontend/src/pages/LiveHub.tsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { supabase } from "../supabaseClient"; 
 import Sidebar from "../components/Sidebar";
-import logo from "./assets/ka-tech-logo.png";
 
 interface Live {
     id: string;
