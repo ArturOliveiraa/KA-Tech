5945481f9dfd3aa6f872d979e1f033bf5c048131
ArturOliveira <121062393+ArturOliveiraa@users.noreply.github.com>
2026-02-18 19:54:56 -0300

    Delete ka-tech-frontend/KA-Tech directory

--- ka-tech-frontend/KA-Tech/ka-tech-frontend/src/utils/formatUrl.ts
+++
@@ -1,9 +0,0 @@
-// src/utils/formatUrl.ts
-export const formatSlug = (title: string) => {
-  return title
-    .toLowerCase()
-    .normalize("NFD")
-    .replace(/[\u0300-\u036f]/g, "")
-    .replace(/[^a-z0-9 ]/g, '')
-    .replace(/\s+/g, '-');
-};
